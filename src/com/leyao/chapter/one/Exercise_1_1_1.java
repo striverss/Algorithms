08bbddb91a02fcc6dc82cbd0c642b47d2af60c84
striverss <striverss@sina.com>
2018-07-04 17:35:29 +0800

    first version

---
+++ src/com/leyao/chapter/one/Exercise_1_1_1.java
@@ -0,0 +1,9 @@
+package com.leyao.chapter.one;
+
+public class Exercise_1_1_1 {
+    public static void main(String[] args) {
+        System.out.println("a: " + ((0 + 15) / 2));
+        System.out.println("b: " + (2.0 * Math.E - 6 * 100000000.1));
+        System.out.println("c: " + (true && false || true && true));
+    }
+}
---
+++ src/com/leyao/test/Test.java
@@ -0,0 +1,10 @@
+package com.leyao.test;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = -14 / 3;
+        int b = 14 / -3;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
