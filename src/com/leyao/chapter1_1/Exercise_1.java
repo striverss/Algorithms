97045809c1c774b9f00509f834bfef56bce321af
striverss <striverss@sina.com>
2018-07-04 17:47:11 +0800

    更新代码结构

--- src/com/leyao/chapter1_1/Exercise_1.java
+++ src/com/leyao/chapter1_1/Exercise_1.java
@@ -1,6 +1,6 @@
-package com.leyao.chapter.one;
+package com.leyao.chapter1_1;
 
-public class Exercise_1_1_1 {
+public class Exercise_1 {
     public static void main(String[] args) {
         System.out.println("a: " + ((0 + 15) / 2));
         System.out.println("b: " + (2.0 * Math.E - 6 * 100000000.1));
